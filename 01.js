1d90fa6ffd58b234e8b60a2aa57d3582b90478e3
satvikraina08 <satvikraina08@gmail.com>
2026-07-29 09:39:38 +0530

    first class

---
+++ 01.js
@@ -0,0 +1,28 @@
+const name = "Satyam" 
+console.log(name);
+
+//let
+//const
+
+
+// object
+
+const a = 25;
+
+console.log(a);
+const s = {
+    name: "RUDE",
+    age: 21,
+};
+s.name = "Samael"
+s.city = "Siwan"
+console.log(s);
+
+
+// array
+
+const f = ["apple", "banana"];
+f.push("mango")
+f[0]="abc"
+
+console.log(f)
